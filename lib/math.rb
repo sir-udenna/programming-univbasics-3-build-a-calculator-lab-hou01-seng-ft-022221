0f84a3db3e683a2ca5d8e94a80264a954932c606
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-02 23:28:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  operation = num1 - num2
+  puts operation
 end
 
 def division(num1, num2)
-
+  operation = num1 / num2
+  puts operation
 end
 
 def multiplication(num1, num2)
-
+  operation = num1 + num2
+  puts operation
 end
 
 def modulo(num1, num2)
-
+  operation = num1 + num2
+  puts operation
 end
 
 def square_root(num)
-
+  operation = num1 + num2
+  puts operation
 end
