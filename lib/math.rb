d72788a9ef8c8ef9546742b744eb0d7760de2882
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-02 23:28:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -14,12 +14,12 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-  operation = num1 + num2
+  operation = num1 * num2
   puts operation
 end
 
 def modulo(num1, num2)
-  operation = num1 + num2
+  operation = num1 % num2
   puts operation
 end
 
